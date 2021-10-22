cdc2c20a9a0ff4b9e07a3ae13d07ecc0b73a3622
Oscar <oscar@plutusds.com>
2021-10-22 18:25:18 +0900

    style setting:

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    'module-resolver',
+    {
+      root: ['./src'],
+      extensions: ['.js', '.ios.js', '.android.js'],
+    },
+  ],
 };
